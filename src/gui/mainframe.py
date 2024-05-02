9938c2786fc5786403379ae68c187d0ba64b81c3
akeegan97 <andrewkeegan97@gmail.com>
2024-05-02 14:44:30 -0400

    styling + increase in path amounts

--- src/gui/mainframe.py
+++ src/gui/mainframe.py
@@ -8,6 +8,7 @@ parent_dir = os.path.abspath(os.path.join(script_dir, '..'))
 sys.path.append(parent_dir)
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
 from tkcalendar import Calendar
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
@@ -19,67 +20,91 @@ import time
 class GBMApp:
     def __init__(self, master):
         self.master = master
-        self.StartDateFrame = tk.Frame(master)
-        self.EndDateFrame = tk.Frame(master)
-        self.PredictionFrame = tk.Frame(master)
+        self.style = ttk.Style(master)
+        self.style_buttons = ttk.Style(master)
+        master.config(bg="#000000")
+        
+        # Configure the custom style for the StartDateFrame
+        self.style.configure("Custom.TFrame", background="#000000", foreground="#05ff3f")
+        self.style_buttons.configure("Button.TButton", font=("Times New Roman", 12), background="#000000", foreground="#05ff3f")
 
-        self.m_ButtonLabel = tk.Label(master, text="Select CSV file:")
-        self.m_FileBrowseButton = tk.Button(master, text="Browse",command=self.browse_file)
 
-        self.m_StartDateLabel = tk.Label(self.StartDateFrame,text="Start Date of Training")
-        self.m_StartDateVerifyLabel = tk.Label(self.StartDateFrame,text="Is Date Valid: ")
+        self.StartDateFrame = ttk.Frame(master)
+        self.EndDateFrame = ttk.Frame(master)
+        self.PredictionFrame = ttk.Frame(master)
 
-        self.m_StartTrainingDate = Calendar(master,selectmode = 'day')
+        self.m_ButtonLabel = ttk.Label(master, text="Select CSV file:")
+        self.m_FileBrowseButton = ttk.Button(master, text="Browse",command=self.browse_file)
+
+        self.m_StartDateLabel = ttk.Label(self.StartDateFrame,text="Start Date of Training")
+        self.m_StartDateVerifyLabel = ttk.Label(self.StartDateFrame,text="Is Date Valid: ")
+
+        self.m_StartTrainingDate = Calendar(master, selectmode='day', background='#000000', foreground='#05ff3f', font=('Times New Roman', 12))
         self.m_StartTrainingDate.bind("<<CalendarSelected>>", self.UpdateStartDateValid)
 
-        self.m_EndDateLabel = tk.Label(self.EndDateFrame, text="End Date of Training")
-        self.m_EndDateVerifyLabel = tk.Label(self.EndDateFrame,text="Is Date Valid: ")
+        self.m_EndDateLabel = ttk.Label(self.EndDateFrame, text="End Date of Training")
+        self.m_EndDateVerifyLabel = ttk.Label(self.EndDateFrame,text="Is Date Valid: ")
 
-        self.m_EndTrainingDate = Calendar(master,selectmode = 'day')
+        self.m_EndTrainingDate = Calendar(master, selectmode='day', background='#000000', foreground='#05ff3f', font=('Times New Roman', 12))
         self.m_EndTrainingDate.bind("<<CalendarSelected>>", self.CombinedCallForBinding)
 
-        self.m_PredictionDaysLabel = tk.Label(self.PredictionFrame,text="Prediction Days")
-        self.m_PredictionStepsValid = tk.Label(self.PredictionFrame,text="Prediction Days Valid: ")
+        self.m_PredictionDaysLabel = ttk.Label(self.PredictionFrame,text="Prediction Days")
+        self.m_PredictionStepsValid = ttk.Label(self.PredictionFrame,text="Prediction Days Valid: ")
         
-        self.m_PredictionDaysSpinBox = tk.Spinbox(master, from_=0,to=0)
+        self.m_PredictionDaysSpinBox = ttk.Spinbox(master, from_=0,to=0)
         self.m_PredictionDaysSpinBox.bind("<KeyRelease>", self.UpdateSteps)
         self.m_PredictionDaysSpinBox.bind("<<Increment>>", self.UpdateSteps)
         self.m_PredictionDaysSpinBox.bind("<<Decrement>>", self.UpdateSteps)
         self.m_PredictionDaysSpinBox.bind("<FocusOut>", self.UpdateSteps)
-        self.m_StartCalculationButton = tk.Button(master, text="Start Prediction",state="disabled",command= self.Calculate)
+        self.m_StartCalculationButton = ttk.Button(master, text="Start Prediction",state="disabled",command= self.Calculate)
 
-        self.m_CSVDataLabel = tk.Label(master,text="Data Name: ")
+        self.m_CSVDataLabel = ttk.Label(master,text="Data Name: ")
         self.m_Data = None
         self.m_StartDate = ""
         self.m_EndDate = ""
         self.m_Steps = ""
-        self.m_Paths = 1_000_000
+        self.m_Paths = 100_000_000
         self.SetupLayout()
 
     def SetupLayout(self):
         self.master.title("GBM Predictions")
+        self.StartDateFrame.config(style="Custom.TFrame")
+        self.PredictionFrame.config(style="Custom.TFrame")
         self.m_ButtonLabel.grid(column=0,row=0)
+        self.m_ButtonLabel.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
         self.m_FileBrowseButton.grid(column=1,row=0)
+        self.m_FileBrowseButton.config(style="Button.TButton")
 
         self.StartDateFrame.grid(column=0,row=1)
         self.m_StartDateLabel.grid(column=0,row=0)
+        self.m_StartDateLabel.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
         self.m_StartDateVerifyLabel.grid(column=0,row=1)
+        self.m_StartDateVerifyLabel.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
 
         self.EndDateFrame.grid(column=0,row=2)
+        self.EndDateFrame.config(style="Custom.TFrame")
         self.m_EndDateLabel.grid(column=0,row=0)
+        self.m_EndDateLabel.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
         self.m_EndDateVerifyLabel.grid(column=0,row=1)
+        self.m_EndDateVerifyLabel.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
 
         self.PredictionFrame.grid(column=0,row=3)
+        self.PredictionFrame.config(style="Custom.TFrame")
         self.m_PredictionDaysLabel.grid(column=0,row=0)
+        self.m_PredictionDaysLabel.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
         self.m_PredictionStepsValid.grid(column=0,row=1)
+        self.m_PredictionStepsValid.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
 
         self.m_StartTrainingDate.grid(column=1,row=1)
         self.m_EndTrainingDate.grid(column=1,row=2)
         self.m_PredictionDaysSpinBox.grid(column=1,row=3)
+        self.m_PredictionDaysSpinBox.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
         
         self.m_StartCalculationButton.grid(column=1,row=4)
+        self.m_StartCalculationButton.config(style="Button.TButton")
 
         self.m_CSVDataLabel.grid(column=2,row=0)
+        self.m_CSVDataLabel.config(font=("Times New Roman",12),background="#000000",foreground="#05ff3f")
 
     def UpdateStartDateValid(self, event = None):
         if self.m_Data is None:
@@ -137,11 +162,13 @@ class GBMApp:
     def plot(self):
         fig = Figure(figsize=(5,5), dpi=100)
         plot1 = fig.add_subplot(111)
-        plot1.plot(self.m_Data['Date'], self.m_Data['Close'])
+        plot1.plot(self.m_Data['Date'], self.m_Data['Close'], color="green", linestyle='-')
+        plot1.set_facecolor("black")
         plot1.set_xlabel('Date')
         plot1.set_ylabel('Close')
         plot1.set_title('Actual Prices')
         plot1.grid(True)
+        
         canvas = FigureCanvasTkAgg(fig, master=self.master)
         canvas.draw()
         canvas.get_tk_widget().grid(column= 2,row=1,rowspan=3,sticky="nsew")
@@ -151,10 +178,11 @@ class GBMApp:
         startIndex = self.m_Data.index[self.m_Data['Date'] == endDate][0]
         realPrices = self.m_Data.iloc[startIndex:startIndex + int(self.m_Steps)]
         plot1.plot(realPrices['Date'], realPrices['Close'], label='Real Prices', color='blue')
+        plot1.set_facecolor("black")
         subsetOfWalks = walks[:min(500, len(walks))]
         for i, walk in enumerate(subsetOfWalks):
             plot1.plot(realPrices['Date'], walk, label=f'Walk {i+1}')
-        plot1.plot(realPrices['Date'],realPrices['Close'],zorder=len(walks)+1,color='black')
+        plot1.plot(realPrices['Date'],realPrices['Close'],zorder=len(walks)+1,color='white')
         plot1.set_xlabel('Date')
         plot1.set_ylabel('Price')
         plot1.set_title('GBM Predictions')
